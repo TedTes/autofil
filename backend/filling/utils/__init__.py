069025a9b0c8a66b1eedf7704c755d146690c3bd
Tedros <tedtfu@gmail.com>
2025-11-16 11:54:17 -0500

    bug fix

--- backend/filling/utils/__init__.py
+++ backend/filling/utils/__init__.py
@@ -3,5 +3,6 @@ Utility functions for filling operations.
 """
 
 from .value_formatter import ValueFormatter
+from .canonical_mapping import canonical_to_template_fields
 
-__all__ = ['ValueFormatter']
+__all__ = ['ValueFormatter','canonical_to_template_fields']

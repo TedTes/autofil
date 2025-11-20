87b5bf47556e66507df3223861a5c7ce9f62e45b
Tedros <tedtfu@gmail.com>
2025-11-19 21:17:21 -0500

    refactoring

--- backend/extraction/mappers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Mappers for transforming raw PDF fields to canonical JSON structure.
-"""
-
-from .acord_126_extraction_mapper import Acord126ExtractionMapper
-
-__all__ = ['Acord126ExtractionMapper']
